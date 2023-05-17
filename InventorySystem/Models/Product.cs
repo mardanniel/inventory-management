b5d621c6b3a61253c8711eb3a9116e1e385b4aeb
mardanniel <mardannielabarondoginturo@gmail.com>
2023-05-17 10:00:57 +0800

    Add project files.

---
+++ InventorySystem/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace InventorySystem.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
